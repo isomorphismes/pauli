21bb4ce9c3511412dc9901c6697a75a8d376c100
i <isomorphisms@sdf.org>
2026-09-26 06:36:42 -0700

    Refactor hydrogen states: add radial.py

---
+++ tools/generate/hydrogen/sage/hydrogen/radial.py
@@ -0,0 +1,51 @@
+from sage.all import (
+    QQ,
+    exp,
+    factorial,
+    gen_laguerre,
+    sqrt,
+)
+
+from .coordinates import radius
+from .quantum_numbers import check_radial_numbers
+
+
+def radial_mode(
+    energy_level,
+    angular_degree,
+):
+    check_radial_numbers(
+        energy_level,
+        angular_degree,
+    )
+
+    scaled_radius = 2 * radius / energy_level
+    normalization = sqrt(
+        (QQ(2) / energy_level) ** 3
+        * factorial(
+            energy_level
+            - angular_degree
+            - 1
+        )
+        / (
+            2
+            * energy_level
+            * factorial(
+                energy_level
+                + angular_degree
+            )
+        )
+    )
+
+    polynomial = gen_laguerre(
+        energy_level - angular_degree - 1,
+        2 * angular_degree + 1,
+        scaled_radius,
+    )
+
+    return (
+        normalization
+        * exp(-radius / energy_level)
+        * scaled_radius**angular_degree
+        * polynomial
+    ).simplify_full()
